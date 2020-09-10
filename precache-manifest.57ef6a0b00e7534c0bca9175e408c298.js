b55cc5d2bf8e69c2060bf4bc461720266dfae6a1
honmapietra <54584172+honmapietra@users.noreply.github.com>
2020-09-10 12:53:45 -0300

    Updates

---
+++ precache-manifest.57ef6a0b00e7534c0bca9175e408c298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80933849f3579f1dff18df2d4096248d",
+    "url": "/prodriguesdev/index.html"
+  },
+  {
+    "revision": "96f9910d9076c5516ad7",
+    "url": "/prodriguesdev/static/css/main.b5667dfc.chunk.css"
+  },
+  {
+    "revision": "8f681848ee8dffb94959",
+    "url": "/prodriguesdev/static/js/2.260d49da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prodriguesdev/static/js/2.260d49da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f9910d9076c5516ad7",
+    "url": "/prodriguesdev/static/js/main.f271683d.chunk.js"
+  },
+  {
+    "revision": "f6e76c0d9449ab36b791",
+    "url": "/prodriguesdev/static/js/runtime-main.af3bb4be.js"
+  }
+]);
